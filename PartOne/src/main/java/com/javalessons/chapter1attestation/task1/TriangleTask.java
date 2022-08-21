b40d532bd918ac364a91f47543635fd27c2801e2
Pavel Kozlovskii <79062502914@yandex.ru>
2022-08-21 17:17:58 +0300

    Attestation task1 sovled

--- PartOne/src/main/java/com/javalessons/chapter1attestation/task1/TriangleTask.java
+++ PartOne/src/main/java/com/javalessons/chapter1attestation/task1/TriangleTask.java
@@ -1,14 +1,49 @@
 package com.javalessons.chapter1attestation.task1;
+
+import java.util.Scanner;
+
 /**
- *  Напишите программу которая принимает 3 числовых значения и проверяет, могут ли они быть
- *  сторонами треугольника.
- *  Для ввода значений с консоли Вам понадобится класс Scanner.
- *  <code>Scanner scanner = new Scanner(System.in);</code>
- *  <code>scanner.nextInt();</code>
+ * Напишите программу которая принимает 3 числовых значения и проверяет, могут ли они быть
+ * сторонами треугольника.
+ * Для ввода значений с консоли Вам понадобится класс Scanner.
+ * <code>Scanner scanner = new Scanner(System.in);</code>
+ * <code>scanner.nextInt();</code>
  */
 public class TriangleTask {
+    public int[] sortSidesList(int[] sides) {
+        int listLength = sides.length;
+        int temporaryVariable;
+        for (byte i = 0; i < listLength - 1; i++) {
+            if (sides[i] > sides[i + 1]) {
+                temporaryVariable = sides[i];
+                sides[i] = sides[i + 1];
+                sides[i + 1] = temporaryVariable;
+            }
+        }
+        return sides;
+    }
 
-    public static void main(String[] args) {
+    public boolean isRealTriangle(int[] sides) {
+        sides = this.sortSidesList(sides);
+        if (sides[2] < sides[0] + sides[1]) {
+            return true;
+        } else return false;
 
     }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter 3 integer numbers");
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        int[] sides = {a, b, c};
+        TriangleTask triangleTask = new TriangleTask();
+        if (triangleTask.isRealTriangle(sides)) {
+            System.out.printf("You can create a triangle with sides %d %d %d\n", a, b, c);
+        } else {
+            System.out.printf("You cannot create a triangle with sides %d %d %d\n", a, b, c);
+        }
+    }
 }
+
