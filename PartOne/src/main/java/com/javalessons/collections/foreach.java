ea7aa8a0ec0e8232d32f94975cfcb6928747b4ee
Pavel Kozlovskii <79062502914@yandex.ru>
2022-09-02 22:32:08 +0300

    Just some code cleaning

--- PartOne/src/main/java/com/javalessons/collections/foreach.java
+++ PartOne/src/main/java/com/javalessons/collections/foreach.java
@@ -4,7 +4,7 @@ public class foreach {
     public static void main(String[] args) {
         int count = 0;
         String[] strings = new String[5];
-        for (int i=0;i<5;i++) {
+        for (int i = 0; i < 5; i++) {
             strings[i] = "This is string number " + count;
             count++;
         }
