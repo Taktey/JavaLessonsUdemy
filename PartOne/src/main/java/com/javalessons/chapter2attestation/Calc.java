98ec96f6635a499b107b6b9acf6661f8dca882dc
Pavel Kozlovskii <79062502914@yandex.ru>
2022-09-04 15:56:33 +0300

    Chapter 2 attestation task completed

--- PartOne/src/main/java/com/javalessons/chapter2attestation/Calc.java
+++ PartOne/src/main/java/com/javalessons/chapter2attestation/Calc.java
@@ -11,7 +11,7 @@ public class Calc {
     private float interest; // credit fee in payment size
     private float principal; // credit body in payment size
     private float endingBalance; // credit amount at the end of the month
-    private float totalInterest =0; // amount of credit fee
+    private float totalInterest = 0; // amount of credit fee
 
     public Calc(int loanAmount, int years, float interestRate) {
         this(loanAmount, years, interestRate, 0);
@@ -22,23 +22,28 @@ public class Calc {
         this.years = years;
         this.interestRate = interestRate;
         this.extraPayment = extraPayment;
+        this.startingBalance = loanAmount;
     }
 
     public void getCreditPlan() {
-        System.out.printf("%S, %15S, %12S, %8S, %8S, %15S, %15S%n",
+        System.out.printf("%S, %15S, %6S, %8S, %8S, %15S, %15S%n",
                 "MONTH", "STARING_BALANCE", "PAYMENT", "INTEREST", "PRINCIPAL",
                 "ENDING_BALANCE", "TOTAL_INTEREST");
         for (int i = 1; i <= years * 12; i++) {
             payment = (startingBalance * (interestRate / 12)) /
-                    (1 - (float) Math.pow((1 + interestRate / 12), years * 12));
+                    (1 - (float) Math.pow((1 + interestRate / 12), -1 * (years * 12 - i)));
             interest = startingBalance * (interestRate / 12);
             principal = payment + extraPayment - interest;
             endingBalance = startingBalance - principal;
-            totalInterest = totalInterest+interest;
-            System.out.printf("%d, %15.2f, %12.2f, %8.2f, %8.2f, %15.2f, %15.2f%n",
+            totalInterest = totalInterest + interest;
+            System.out.printf("%2d %18.2f %10.2f %8.2f %10.2f %15.2f %15.2f%n",
                     i, startingBalance, payment, interest, principal,
                     endingBalance, totalInterest);
-
+            startingBalance = endingBalance;
+            if (endingBalance <= 0) {
+                System.out.printf("Early repayment of the loan, %d month", years * 12 -i);
+                break;
+            };
         }
     }
 }
