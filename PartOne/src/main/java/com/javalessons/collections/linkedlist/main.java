aea921c85a50866e00ccdd0fb0673caf108d7045
Pavel Kozlovskii <79062502914@yandex.ru>
2022-09-07 21:52:29 +0300

    Task: write LinkedList with methods realization

---
+++ PartOne/src/main/java/com/javalessons/collections/linkedlist/main.java
@@ -0,0 +1,34 @@
+package com.javalessons.collections.linkedlist;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class main {
+    public static void main(String[] args) {
+        LinkedList<String> list = new LinkedList<>();
+        list.add("String1");
+        list.add("String2");
+        list.add("String3");
+        System.out.println(list);
+        list.add(1, "New String2");
+        System.out.println(list);
+        list.remove(2);
+        System.out.println(list);
+        System.out.println(list.size());
+        System.out.println(list.contains("String1"));
+        list.addFirst("String First");
+        list.addLast("String Last");
+        System.out.println(list.peekFirst()+", "+ list.peekLast());
+        System.out.println(new LinkedList<>());
+        System.out.println(list.pollFirst());
+        System.out.println(list.pollLast());
+        System.out.println(list);
+
+        String[] array = list.toArray(new String[3]);
+        for(String string: array){
+            System.out.print(string+", ");
+        }
+        
+
+    }
+}
